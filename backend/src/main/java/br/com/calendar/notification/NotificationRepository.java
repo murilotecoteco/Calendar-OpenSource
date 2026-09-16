2dae8bde63ff864993578e30c9ea4401eba2b216
ThurDas <va4591073@gmail.com>
2026-09-16 15:51:40 -0300

    feat(notification): add read state repository updates

--- backend/src/main/java/br/com/calendar/notification/NotificationRepository.java
+++ backend/src/main/java/br/com/calendar/notification/NotificationRepository.java
@@ -1,6 +1,22 @@
 package br.com.calendar.notification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, String> {
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.read = true "
+            + "WHERE n.id = :notificationId "
+            + "AND n.user.id = :userId "
+            + "AND n.read = false")
+    int markUnreadAsRead(@Param("notificationId") String notificationId, @Param("userId") String userId);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.read = true "
+            + "WHERE n.user.id = :userId "
+            + "AND n.read = false")
+    int markAllUnreadAsRead(@Param("userId") String userId);
 }
